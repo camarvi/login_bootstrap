bdc75df753fb678a9c50fdf6dbdfeaa2eb63a713
carlos <carlos@sobremesa>
2017-10-21 21:52:32 +0200

    Ejemplo login y formulario Bootstrap

---
+++ index_1.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+
+<htm>
+    
+    <head>
+      
+    <meta charset="utf-8">
+    <title>Bootstrap, from Twitter</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+    
+
+    <script src="js/bootstrap.min.js"></script>
+    </head>    
+    
+    <body>  
+
+       
+
+  <form class="form-horizontal" role="form">
+  
+  <div class="form-group">
+    <label for="ejemplo_password_3" class="col-lg-1 control-label">NOMBRE</label>
+    <div class="col-lg-2">
+      <input type="text" class="form-control" id="ejemplo_password_3" 
+             placeholder="">
+    </div>
+    <label for="ejemplo_password_3" class="col-lg-1 control-label">APELLIDOS</label>
+    <div class="col-lg-2">
+      <input type="password" class="form-control" id="ejemplo_password_3" 
+             placeholder="">
+    </div>
+    <label for="ejemplo_password_3" class="col-lg-1 control-label">NUHSA</label>
+    <div class="col-lg-2">
+      <input type="password" class="form-control" id="ejemplo_password_3" 
+             placeholder="">
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="col-lg-offset-2 col-lg-10">
+      <div class="checkbox">
+        <label>
+          <input type="checkbox"> No cerrar sesión
+        </label>
+      </div>
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="col-lg-offset-2 col-lg-10">
+      <button type="submit" class="btn btn-default">Entrar</button>
+    </div>
+  </div>
+</form>
+
+ 
+        
+        
+        
+        
+    </body>   
+</htm>
